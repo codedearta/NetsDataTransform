f274995f582827c232357f34a4e13557980f367f
mdbconsult <33570573+mdbconsult@users.noreply.github.com>
2021-01-22 14:55:47 +0000

    removed unused apps

--- redpRealApp_13/functions/purchaseDateToDayInteger/source.js
+++
@@ -1,6 +0,0 @@
-exports = function(purchaseDate) {
-  return purchaseDate.getFullYear() * 10000 + (purchaseDate.getMonth() + 1) * 100 + purchaseDate.getDate();
-  // const purchaseDateParts = purchaseDate.split(" ")[0].split("-");
-  // const ldayInteger = parseInt(purchaseDateParts[2]+""+purchaseDateParts[1]+""+purchaseDateParts[0],10);
-  // return ldayInteger;
-};
